edb5833465dceb43b56d15295ab02f6f3f765327
Kit Tejido <kittejido@gmail.com>
2021-03-01 13:53:56 -0700

    created a separate script.js file

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var rock = "rock";
+var paper = "paper";
+var scissors = "scissors";
+
+var computerWeapons = [rock, paper, scissors];
+
+var playerWeapons = [rock, paper, scissors];
+
+function selectElement(element) {
+    
+}
