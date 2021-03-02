d7e6948975feea70fd23cdd445378f07308c7ff5
Kit Tejido <kittejido@gmail.com>
2021-03-01 21:47:23 -0700

    placing function within a function

--- script.js
+++ script.js
@@ -10,8 +10,30 @@ var scissors = "scissors";
 
 var computerWeapons = [rock, paper, scissors];
 
-// function that creates random numbers from array elements
+// ### function that creates random numbers from array elements ###
 function selectElement(element) {
   const random = Math.floor(Math.random() * computerWeapons.length);
   // alert(random);
+
+// ### placing function within a function ###
+const convertRandomToString = function (random) {
+
+// ### convert integer to string ###
+    switch (random) {
+      case 0 :
+        return rock;
+        break;
+        
+      case 1 :
+        return paper;
+        break;
+        
+      case 2 :
+        return scissors;
+        break;
+        
+      default :    
+    }
+ }
+
 }
