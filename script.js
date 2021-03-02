6648b327daa464beac7ff44f867f488be07bdab0
Kit Tejido <kittejido@gmail.com>
2021-03-01 22:00:42 -0700

    access newly converted strings from array

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ const convertRandomToString = function (random) {
     }
  }
 
-// ### another way of anonymous function within a function ###
+// ### another way of anonymous function within a function but using the arrow function instead ###
 //   const convertRandomToString = (random) => {
 
 //   convert integer to string
@@ -59,4 +59,9 @@ const convertRandomToString = function (random) {
 //   }
 
 
+// ### Accessing newly converted random strings from array and assigning it to a new variable ###
+const computerPlayer = computerWeapons[random];
+
+// OR us below
+// const computerPlayer = convertRandomToString(random);
 }
